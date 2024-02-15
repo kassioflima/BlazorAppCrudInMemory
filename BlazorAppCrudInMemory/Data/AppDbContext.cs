52d9affdf247fb331bcdbe3d2dee6af0571fe2af
Kássio Fernandes <kassioflima@outlook.com>
2024-02-15 15:25:54 -0300

    Add project files.

---
+++ BlazorAppCrudInMemory/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorAppCrudInMemory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppCrudInMemory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
---
+++ BlazorAppCrudInMemory/Program.cs
@@ -0,0 +1,34 @@
+using BlazorAppCrudInMemory.Components;
+using BlazorAppCrudInMemory.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseInMemoryDatabase("app"));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
